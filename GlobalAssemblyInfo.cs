4d9df160284c2fdaf41c8423af26cbda584d0c1d
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-08-27 16:11:27 +0200

    Initial form ILProj

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("Đonny")]
-[assembly: AssemblyProduct("ILProj")]
+[assembly: AssemblyProduct("VSLangProj.Shared")]
 [assembly: AssemblyCopyright("Copyright © Đonny 2015")]
 
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
