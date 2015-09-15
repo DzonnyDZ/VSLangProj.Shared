a7ded64785f9bf59ecb4af76736a6e5211afab0e
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-09-15 19:45:26 +0200

    NuGet package generation

--- VSLangProj.Shared/Properties/AssemblyInfo.cs
+++ VSLangProj.Shared/Properties/AssemblyInfo.cs
@@ -1,13 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: Microsoft.VisualStudio.Shell.ProvideCodeBase(CodeBase = @"$PackageFolder$\Dzonny.ILProj.dll")]
 
 [assembly: AssemblyTitle("VSLangProj.Shared")]
-[assembly: AssemblyDescription("Shared finctionality for Visual Studio Custom Project System -based exetnsions")]
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6e82e795-156f-4b68-885d-83f2a507d9ad")]
+[assembly: AssemblyDescription("Shared functionality for Visual Studio Custom Project System -based extensions")]
